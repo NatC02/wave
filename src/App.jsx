663f9d8db4ecc669083265b0e44e66ef02c94479
NatC02 <natanceballos02@gmail.com>
2025-04-13 23:31:42 -0400

    render entry through App jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+
+function App() {
+  return (
+    <Canvas camera={{ position: [3, 3, 3], fov: 30 }}>
+      <Experience />
+    </Canvas>
+  );
+}
+
+export default App;
